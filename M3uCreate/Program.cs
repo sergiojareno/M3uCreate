f4a2932d1ae9d0ded7d81c6209281862af632ddf
Sergio Jareño <sergioj@siainteractive.com>
2016-01-26 09:25:01 -0300

    Full project commit

---
+++ M3uCreate/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace M3uCreate
+{
+    class Program
+    {
+        static readonly Encoding encoding = Encoding.GetEncoding(1252);
+
+        static void Main()
+        {
+            string path = Directory.GetCurrentDirectory();
+            ProcessDirectory(path);
+        }
+
+        private static void ProcessDirectory(string path)
+        {
+            Console.WriteLine("Processing " + path);
+
+            // Delete existing M3U
+            foreach (var file in Directory.GetFiles(path, "*.m3u")) File.Delete(file);
+
+            // Build the list of audio files
+            var regexAudioFiles = new Regex(@"\.(mp3|wma|wav|ogg|mid|midi|flac|m4a|voc|aiff)$", RegexOptions.IgnoreCase);
+            var m3UFileName = Path.Combine(path, Path.GetFileName(path) + ".m3u");
+            StreamWriter writer = null;
+            try
+            {
+                foreach (var file in Directory.GetFiles(path, "*.*").Where(f => regexAudioFiles.IsMatch(f)).OrderBy(f => f))
+                {
+                    //Lazy creation of writer so we don't create empty m3u files in directories with no audio files
+                    if (writer == null) writer = new StreamWriter(m3UFileName, false, encoding);
+                    writer.WriteLine(Path.GetFileName(file));
+                }
+            }
+            finally
+            {
+                if (writer != null) writer.Close();
+            }
+
+            // Process subdirectories
+            foreach (var file in Directory.GetDirectories(path)) ProcessDirectory(file);
+
+        }
+
+
+    }
+}
